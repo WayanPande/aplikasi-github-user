b3a96a008db622caf0df3801b301f510111bbf1e
WayanPande <yande554@gmail.com>
2022-03-11 23:39:19 +0700

    adding searchView on navigation bar

--- app/src/main/java/com/example/aplikasigithubuser/MainActivity.kt
+++ app/src/main/java/com/example/aplikasigithubuser/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.aplikasigithubuser
 
+import android.annotation.SuppressLint
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.aplikasigithubuser.databinding.ActivityMainBinding
 
@@ -22,7 +28,33 @@ class MainActivity : AppCompatActivity() {
         showRecycleList()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.search)?.actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+
+        return true
+    }
+
     private val listUsers: ArrayList<User>
+        @SuppressLint("Recycle")
         get() {
             val dataUsername = resources.getStringArray(R.array.username)
             val dataName = resources.getStringArray(R.array.name)
